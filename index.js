3b29859480e068d026d83d763f0150c850c6741a
Ragline <132845758+Ragline@users.noreply.github.com>
2024-12-24 02:15:41 +0530

    added passenger counter

---
+++ index.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+let countEl = document.getElementById("count-el")
+
+let saveEl = document.getElementById("save-el")
+
+function increment (){
+    count += 1;
+    countEl.textContent = count;
+     
+}
+
+function save() {
+    updatedCount = " " + count + " - "
+    saveEl.textContent += updatedCount;
+    countEl.textContent = 0;
+    count = 0;
+
+}
